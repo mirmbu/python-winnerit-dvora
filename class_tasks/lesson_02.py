11e9c60a6655d87b0561f09f39f6b9ce246fd916
mirmbu <dvoraack@gmail.com>
2025-04-01 21:56:15 +0300

    class task lesson 02

---
+++ class_tasks/lesson_02.py
@@ -0,0 +1,14 @@
+#task 1
+num = int(input("Enter a number: "))
+print(num * 2)
+print(num / 2)
+
+#taks 2
+width = int(input("Enter width of rectangle: "))
+height = int(input("Enter height of rectangle: "))
+print("The erea is: ", width * height)
+print("The perimeter is: ", (width + height)*2)
+
+#task 3
+temp = int(input("Enter a number: "))
+print((temp - 32)*5 / 9)
---
+++ lesson_02/input_example.py
@@ -0,0 +1,27 @@
+# name = input("Enter your name: ")
+# print(name)
+
+# age = input("Enter you age: ")
+# print(age)
+
+#string
+# num_a = int(input("Enter number 1: "))
+# num_b = int(input("Enter number 2: "))
+# print(num_a + num_b)
+
+#convert to int
+# num_a = int(input("Enter number 1: "))
+# num_b = int(input("Enter number 2: "))
+# print(num_a + num_b)
+
+#convert to float
+# num_a = float(input("Enter number 1: "))
+# num_b = int(input("Enter number 2: "))
+# print(num_a + num_b)
+
+numbers = input("Enter 2 numbers seperated by space: ").split()
+print(numbers)
+number_1 = int(numbers[0])
+number_2 = int(numbers[1])
+print(number_1)
+print(number_2)
