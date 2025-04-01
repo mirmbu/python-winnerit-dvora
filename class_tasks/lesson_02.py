9d45b5925b72c59635bbc30f0537c9ebcbc242e6
mirmbu <dvoraack@gmail.com>
2025-04-01 22:14:51 +0300

    adding files

--- class_tasks/lesson_02.py
+++ class_tasks/lesson_02.py
@@ -1,14 +1,19 @@
-#task 1
-num = int(input("Enter a number: "))
-print(num * 2)
-print(num / 2)
+##task 1
+#num = int(input("Enter a number: "))
+#print(num * 2)
+#print(num / 2)
+#
+##taks 2
+#width = int(input("Enter width of rectangle: "))
+#height = int(input("Enter height of rectangle: "))
+#print("The erea is: ", width * height)
+#print("The perimeter is: ", (width + height)*2)
+#
+##task 3
+#temp = int(input("Enter a number: "))
+#print((temp - 32)*5 / 9)
 
-#taks 2
-width = int(input("Enter width of rectangle: "))
-height = int(input("Enter height of rectangle: "))
-print("The erea is: ", width * height)
-print("The perimeter is: ", (width + height)*2)
-
-#task 3
-temp = int(input("Enter a number: "))
-print((temp - 32)*5 / 9)
+#task 4
+amount_of_money = float(input("Enter a number: "))
+amount_of_money*=1.5
+print(round(amount_of_money, 2))
---
+++ lesson_02/assingment_operators.py
@@ -0,0 +1,29 @@
+a = 5
+print(a)
+print(a+1)
+print(a)
+
+a = a + 1
+print(a)
+a += 1
+# a++ -> not valid in python
+print(a)
+
+a -= 2
+print(a)
+
+a *= 20
+print(a)
+
+a /=10
+print(a)
+
+a //= 3
+print(a)
+
+a **= 3
+print(a)
+
+a %= 3
+print(a)
+
