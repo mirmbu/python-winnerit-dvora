77723e39b209b25160019f3947a31ca8f7dff801
mirmbu <dvoraack@gmail.com>
2025-04-22 17:54:48 +0300

    adding home work

---
+++ home_tasks/home_task_lesson_2.py
@@ -0,0 +1,16 @@
+#Task 1
+x1 = float(input("Enter point 1: "))
+x2 = float(input("Enter point 2: "))
+
+distance = x2 - x1
+print("The distance between point 1 to point 2 is: ", distance)
+
+#Task 2
+gm = int(input("Enter number of grams: "))
+kg = gm / 1000
+print("The kg is: ", int(kg))
+
+#Task 4
+num = int(input("Enter a number: "))
+num = num % 10
+print("The last digit is: ", num)
