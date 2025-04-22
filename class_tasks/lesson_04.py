d1b16463839cada49f41144cd790aeb40611ba6b
mirmbu <dvoraack@gmail.com>
2025-04-22 21:25:35 +0300

    add class task lesson 04

---
+++ class_tasks/lesson_04.py
@@ -0,0 +1,11 @@
+#Task 1
+string = str(input("Enter a string: "))
+print(string[:4])
+
+#Task 2
+word = str(input("Enter a word: "))
+print(word[-1] + word[:-1])
+
+#Task 3:
+string2 = str(input("Enter a string: "))
+print(string2[1::2])
