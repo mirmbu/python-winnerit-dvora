2ac7b862b913a0adb9a18bd53009dfb5d62cd6bb
mirmbu <dvoraack@gmail.com>
2025-04-23 22:39:33 +0300

    add home work lesson 04

---
+++ home_tasks/lesson_03.py
@@ -0,0 +1,27 @@
+#Task 1
+#string = str(input("Enter a string: "))
+#
+#print(string[0])
+#print(string[-1])
+#print(string[len(string)//2])
+#print(string[0::2])
+#print((string[1::2]))
+#print(string[::-1])
+from operator import index
+
+#Task 2
+#string2 = str(input("Enter a message: "))
+#
+#print(string2.lower())
+#print(string2.upper())
+#print(string2[0].upper() + string2[1:].lower())
+#print(string2.title())
+#words = string2.split()
+#print(words)
+
+#Task 3
+width = int(input("Enter screen width in pixels: "))
+height = int(input("Enter screen height in pixels: "))
+
+print(f"Resolution: {width} * {height}")
+print(f"Total pixels: {width * height}")
