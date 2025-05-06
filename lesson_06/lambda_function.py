148bd5fa3c9ef24bc4cd25c33322e8c7b9b42bf6
mirmbu <dvoraack@gmail.com>
2025-05-06 22:01:40 +0300

    lesson 06

--- lesson_06/lambda_function.py
+++ lesson_06/lambda_function.py
@@ -1,55 +1,67 @@
-# #lambda function is anonymous function.
-# calculation = lambda x: x ** 3
-# result = calculation(3)
-# print(result)
-#
-# calculation2 = lambda x, y: x ** y
-# result2 = calculation2(5, 3)
-# print(result2)
-#
-# def calculation2_not_lambda(x, y):
-#     result3 = x ** y
-#     return result3
-# print(calculation2_not_lambda(5,3))
+# lambda
+# A lambda function (or anonymous function) is a small, anonymous function that can be defined in one line.
+# Can have any number of arguments but can only have one expression.
+# Need to assign it, because if not, we don't have a name to refer to it.
+# Example:
+#  This alone doesn't work - the lambda is created but can't be called
+lambda x: x ** 3
+
+# This works because we store the lambda in a variable
+calculation = lambda x: x ** 3
+result = calculation(3) #Output = 27
+print(result)
+
+# This also works because we immediately print the lambda function.
+print((lambda x: x ** 3)(3)) #Output = 27
 
 
-# #map() with lambda
-# #map is function that do any action on every element in a list.
-# #used usually in lambda expression.
-# #example with lambda -> square each element in the list.
-# numbers = [1,2,3,4,5]
-# squared_numbers = list(map(lambda x: x ** 2, numbers))
-# print(squared_numbers)
-#
-# #same exemple with map() that call a function.
-# def squared_number2(x: int):
-#     return x ** 2
-#
-# result = list(map(squared_number2, numbers))
-# print(result)
-#
-# #function that do same thing as map.
-# def work_with_list(list_of_nums: list[int]):
-#     new_list = []
-#     for num in list_of_nums:
-#         new_list.append(num ** 2)
-#     return new_list
-# print(work_with_list(numbers))
+def calculation_not_lambda(x, y):
+    result_not_lambda = x ** y
+    return result_not_lambda
+print(calculation_not_lambda(3,3)) #Output = 27
 
 
-#filter
-#return all the elements that answer on the condition
+# map() with lambda
+# map is a function that does any action on each element in a list.
+# can get a list or call a function.
+# used usually in lambda expression.
+# an example with lambda -> square each element in the list.
+numbers = [1,2,3,4,5]
+squared_numbers = list(map(lambda x: x ** 2, numbers))
+print(squared_numbers)
+
+# same exemple with map() that call a function.
+def squared_number2(x: int):
+    return x ** 2
+
+result = list(map(squared_number2, numbers))
+print(result)
+
+# a function that does the same thing as map() function.
+def work_with_list(list_of_nums: list[int]):
+    new_list = []
+    for num in list_of_nums:
+        new_list.append(num ** 2)
+    return new_list
+print(work_with_list(numbers))
+
+
+# filter
+# is a function that filters elements in a list by a condition.
+# example with lambda -> filter out all elements in the list that are less than 60.
 num_to_filter = [60, 80, 90, 50, 45, 70, 90, 95, 70, 61, 30]
-
-def filter_numbers(x: int):
-    return x >= 60
 
 filter_result = list(filter(lambda x: x>= 60, num_to_filter))
 print(filter_result)
 
+# same example with lambda that call a function.
+def filter_numbers(x: int):
+    return x >= 60
+
 filter_result2 = list(filter(filter_numbers, num_to_filter))
 print(filter_result2)
 
+# a function that does the same thing as filter() function.
 def work_with_list(list_of_nums: list[int]):
     new_list = []
     for num in list_of_nums:
--- lesson_06/list_comprehension.py
+++ lesson_06/list_comprehension.py
@@ -1,10 +1,14 @@
+# list comprehension.
+# a way to short way to create a list, based on a provided list.
+# it combinates for loop and if condition.
+# first part -> expression, what to do.
+# middle part -> for loop on the list.
+# last part -> if condition on each element. (optional)
+# return all the elements that answer the condition.
+# for example:
+
 numbers = [1,2,3,4,5,6,7,8,9]
 numbers2 = [60, 80, 90, 50, 45, 70, 90, 95, 70, 61, 30]
-
-#first - expression - what to do.
-#middle - for loop on the list.
-#last - if condition on each element.
-#return all the element that answer on the condition.
 
 #like map()
 new_list1 = [item ** 2 for item in numbers]
@@ -15,8 +19,8 @@ print(new_list1)
 new_list2 = [num for num in numbers2 if num >= 60]
 print(new_list2)
 
-#if the condition is the middle - mean return all the elements, but the expression will be on the element that answer the condition.
-#also, need else.
+#if the condition part is the middle - mean return all the elements, but the expression will be on the element that answer the condition.
+#in those cases, need else.
 
 new_list3 = [num ** 2 if num % 2 == 0 else num ** 0 for num in numbers]
 print(new_list3)
